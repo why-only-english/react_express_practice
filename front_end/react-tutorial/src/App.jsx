9aa179e0bf07726b353c5a04ffd73acfb416e442
jgmoon <6813821@tukorea.ac.kr>
2024-10-23 14:19:20 +0900

    Creating a Todo App

--- front_end/react-tutorial/src/App.jsx
+++ front_end/react-tutorial/src/App.jsx
@@ -1,12 +1,4 @@
-import HelloWorld, { HelloWorld2 } from './components/HelloWorld';
-import CaptionImage from './components/CaptionImage';
-import MyButton from './components/MyButton';
-import BlinkComponent from './components/BlinkComponent';
-import CountComponent from './components/CountComponent';
-import OnChangeInput from './components/OnChangeInput';
-import ForbiddenWords from './components/ForbiddenWords';
-
-import TimerWithInput from './components/TimerWithInput';
+import TodoApp from './components/TodoApp';
 
 function App() {
   return (
@@ -15,11 +7,15 @@ function App() {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '100vh',
         flexDirection: 'column',
+        gap: '20px',
+        padding: '20px',
+        backgroundColor: '#d0e8ff',
+        height: '100vh',
       }}
     >
-      <TimerWithInput /> {/* 타이머 컴포넌트 렌더링 */}
+      {/* TodoApp 컴포넌트 */}
+      <TodoApp />
     </div>
   );
 }
