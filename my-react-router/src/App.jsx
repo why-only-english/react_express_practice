66fd8a508f4cfb7085fbbe39301048b3d50da8bb
jgmoon <6813821@tukorea.ac.kr>
2024-11-04 09:28:32 +0900

    Install eslint-plugin-prettier

--- my-react-router/src/App.jsx
+++ my-react-router/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+// import "./App.css";
 
-import mainRouter from "./routers/main-router";
-import { RouterProvider } from "react-router-dom";
+import mainRouter from './routers/main-router';
+import { RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-
   return (
     <>
       <RouterProvider router={mainRouter} />
--- my-react-router/src/routes/board/page.jsx
+++ my-react-router/src/routes/board/page.jsx
@@ -1,8 +1,11 @@
+import MyNavbar from '../../../components/MyNavbar/MyNavbar';
+
 export default function BoardPage() {
-    return (
-      <div>
-        <h1>BoardList</h1>
-        <p>This is BoardListPage</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <MyNavbar brandTitle="My-React-Board" />
+      <h1>BoardList</h1>
+      <p>This is BoardListPage</p>
+    </div>
+  );
+}
